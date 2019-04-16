85a310b1eda8c3f449d84fc1fb8baf8d46365ffa
luctery <luctery@foxmail.com>
2019-04-16 14:21:12 +0800

    替换校验算法为CRC16/CCITT

--- app/src/main/java/cn/com/start/cloudprinter/startcloudprinter/handler/AbsHandler.java
+++ app/src/main/java/cn/com/start/cloudprinter/startcloudprinter/handler/AbsHandler.java
@@ -13,6 +13,7 @@ import cn.com.start.cloudprinter.startcloudprinter.handler.netty.DeviceOrder;
 import cn.com.start.cloudprinter.startcloudprinter.po.OrderObj;
 import cn.com.start.cloudprinter.startcloudprinter.tool.verify.IVerify;
 import cn.com.start.cloudprinter.startcloudprinter.tool.verify.VerifyType;
+import cn.com.start.cloudprinter.startcloudprinter.tool.verify.impl.CRC16CCITTVerify;
 import cn.com.start.cloudprinter.startcloudprinter.tool.verify.impl.CRC16XModemVerify;
 import cn.com.start.cloudprinter.startcloudprinter.util.ToolUtil;
 import io.netty.channel.Channel;
@@ -28,7 +29,7 @@ public abstract class AbsHandler {
 
     protected AbsHandler mNextHandler;
     protected InputStream inputStream;
-    protected IVerify mVerifyTool = new CRC16XModemVerify();
+    protected IVerify mVerifyTool = new CRC16CCITTVerify();
     protected Channel mChannel;
 
     public final void handleRequest(ChannelHandlerContext channelHandlerContext, DeviceOrder deviceOrder){
--- app/src/main/java/cn/com/start/cloudprinter/startcloudprinter/tool/verify/VerifyType.java
+++ app/src/main/java/cn/com/start/cloudprinter/startcloudprinter/tool/verify/VerifyType.java
@@ -7,7 +7,8 @@ public enum VerifyType {
     MD5((byte)0x01),
     CRC8((byte)0x02),
     CRC16((byte)0x03),
-    CRC32((byte)0x04);
+    CRC32((byte)0x04),
+    CRCCCITT((byte)0x05);
 
     @Getter
     byte type;
